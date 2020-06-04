36bbf02a96d8fa15c293e3173375b329f3bb1135
Ulises Saavedra <ulises_2692@hotmail.com>
2020-06-03 23:00:03 -0500

    First URL added and view configuration

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,12 @@
+from django.urls import resolve
 from django.test import TestCase
+from lists.views import home_page
+from django.shortcuts import render
+from django.urls import path
 
-class SmokeTest (TestCase):
-    def test_bad_maths(self):
-        self.assertEqual(1+1, 3)
+
+class HomePageTest (TestCase):
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('Home/')
+        self.assertEqual(found.func, home_page)
+        home_page = None
--- superlist/urls.py
+++ superlist/urls.py
@@ -1,21 +1,16 @@
-"""superlist URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path('lists/', include('lists.urls')),
     path('admin/', admin.site.urls),
 ]
+
+"""
+str - Matches any non-empty string, excluding the path separator, '/'. This is the default if a converter isn’t included in the expression.
+int - Matches zero or any positive integer. Returns an int.
+slug - Matches any slug string consisting of ASCII letters or numbers, plus the hyphen and underscore characters. For example, building-your-1st-django-site.
+uuid - Matches a formatted UUID. To prevent multiple URLs from mapping to the same page, dashes must be included and letters must be lowercase. For example, 075194d3-6885-417e-a8a8-6c931e272f00. Returns a UUID instance.
+path - Matches any non-empty string, including the path separator, '/'. This allows you to match against a complete URL path rather than a segment of a URL path as with str
+
+"""
